baa4d7a05077eeeeaa4082c62f2ddb0538a542db
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-09-14 16:18:19 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const app = express();
+
+const userModel = require('./usermodel');
+
+app.get("/",(req , res)=>{
+    res.send("Hello");
+});
+
+// app.get("/create",(req , res)=>{
+//     userModel.create({            // This code is asynchronous .
+//         name : "Ritik" ,
+//         username : "Rahul" ,
+//         email : "ritik@gmail.com"  ,
+//     });
+//     console.log(" Hey ");         // Here hey execute first. 
+// });
+
+// === In just above commented code if we want to execute/print asynchronous code then for this we have two way .===========
+
+// ------------------ First way to execute asynchronous code -----------------------
+
+// ===== create ..................
+
+app.get("/create",async(req , res)=>{
+    let createduser = await userModel.create({            // This code is asynchronous . now execute first .
+        name : "Ritik" ,
+        username : "Rahul" ,
+        email : "ritik@gmail.com"  ,
+    });
+    res.send(createduser);
+    console.log(" Hey ");   // Here hey execute after asynchronous code . 
+});
+
+// ===== read...................
+
+app.get("/read",async(req , res)=>{
+
+    let users = await userModel.find();
+
+    // give blank array......
+    // let users = await userModel.find({name : "rr"}); 
+
+    // only one read......
+    // let users = await userModel.find({name : "Rahul chaurasia"}); 
+
+    res.send(users);
+});
+
+// ===== update .................
+
+app.get("/update",async(req , res)=>{
+    // let createduser = await userModel.findOneUpdate(findone , update , {new : true});
+    let updatedUser = await userModel.findOneAndUpdate({username:"Rahul"} , {name:"Rahul chaurasia"} , {new : true});
+
+    res.send(updatedUser);
+});
+
+// ====  delete .....................
+
+app.get("/delete",async(req , res)=>{
+    
+   let users = await userModel.findOneAndDelete({name:"Ritik"});
+
+    res.send(users);
+});
+
+
+
+app.listen(3000,()=>{
+    console.log("The code is running on port 3000");
+});
---
+++ usermodel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb://127.0.0.1:27017/mongopractice`);
+
+const userSchema = mongoose.Schema({   // Here Schema is a method that conatin information of user as an object.
+    name : String ,
+    username : String ,
+    email : String ,
+});
+
+// If we work on route then it is necessary to export the model. export is a property  not method. 
+module.exports = mongoose.model("user" , userSchema);  
