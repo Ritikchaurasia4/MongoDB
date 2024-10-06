6c8881718d350d8cff5cdde9f9a4814d5785e293
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-10-06 08:52:32 +0530

    create models for  owners

--- Miniproject1/app.js
+++ Miniproject1/app.js
@@ -21,8 +21,8 @@ app.get("/" , (req , res)=>{
 });
 
 app.post("/register" , async(req , res)=>{
-    const{name , username , email , password , age} = req.body;  // Destructuring
-    const user = await userModel.findOne({email});  // Here  we are checking that account is pre login or not with the email .
+    let{name , username , email , password , age} = req.body;  // Destructuring
+    let user = await userModel.findOne({email});  // Here  we are checking that account is pre login or not with the email .
     if(user) return res.status(500).send("yes user is already registered");
 
     // create the user
@@ -37,7 +37,7 @@ app.post("/register" , async(req , res)=>{
                 age ,
             });
 
-            let token = jwt.sign("email" , "secretPasswordHere");
+            let token = jwt.sign({email: email, userid: user._id} , "secretPasswordHere");
             res.cookie("token" , token);
             res.send("Registered");
         })
@@ -51,12 +51,19 @@ app.get("/login" , (req , res)=>{
 
 // here you can login using a valid email and password otherwise you can not login
 app.post("/login" , async(req , res)=>{
-    let{ email , password} = req.body; 
+    // here we can login with email and password only
+    let{ email, password} = req.body; 
     let user = await userModel.findOne({email});  
     if(!user) return res.status(500).send("Something went wrong");
 
-    bcrypt.compare(password , user.password , (err ,result)=>{
-        if(result) res.status(200).send("you can login");
+    // Here may be the user put the wrong password for login .
+    bcrypt.compare(password , user.password , (err, result)=>{
+        if(result) {
+            let token = jwt.sign({email: email, userid: user._id} , "secretPasswordHere");
+            res.cookie("token" , token);
+            res.status(200).send("you can login");
+            
+        }
         else res.redirect("/login");
     })
    
@@ -66,24 +73,56 @@ app.post("/login" , async(req , res)=>{
 
 app.get("/logout" ,(req , res)=>{
     res.cookie("token" , " ");
+    // res.cookie("token", "", { expires: new Date(0) });
+
     res.redirect("/login");
 })
 
 app.get("/profile" , isLoggedIn , (req , res)=>{
     console.log(req.user);
-    res.render("login");
+    // res.render("login");
+    res.render("profile");
+
 });
 
-// create a middlewere 
-function isLoggedIn(req , res , next){
-    if(req.cookies.token === "") res.send("You must be loggedin");
+// create a middlewere . when we will have to login then we will meet this below middlewere.
+function isLoggedIn(req, res, next){
+    // if(req.cookies.token === "")  res.send("You must be loggedin");
+    if(req.cookies.token === "")  res.redirect("/login");
+
     else{
         let data = jwt.verify(req.cookies.token , "secretPasswordHere");
         req.user = data;
+        next();
     }
-    next();
+    // console.log(req.cookies);
+    
 }
+
+//================ or , ==============================
+
+// app.get("/profile", isLoggedIn, (req, res) => {
+//     console.log(req.user);
+//     res.render("profile");
+// });
+
+// //Middleware
+// function isLoggedIn(req, res, next) {
+//     // if (!req.cookies.token) return res.send("You must be logged in");
+//     if (!req.cookies.token) return res.redirect("/login");
+
+    
+//     try {
+//         let data = jwt.verify(req.cookies.token, "secretPasswordHere");
+//         req.user = data;
+//         next();
+//     } catch (err) {
+//         return res.status(401).send("Invalid token");
+//     }
+// }
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 })
+
+
